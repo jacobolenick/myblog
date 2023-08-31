ba8e427e75c0d232c08c940d39a7eba94f0e0732
Jacob Olenick <olenickjacob@gmail.com>
2023-08-31 15:41:08 -0700

    I updated the preview text for the blog posts

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import Blogcard from './Components/Blogcard';
 
 const blogs = [
-  {title: "Why I'm teaching myself React", body: "This is my first blog post!"},
-  {title: "My first week learning ReactJS", body: "This is my first blog post!"},
-  {title: "I built a simple blog", body: "This is my first blog post!"},
-  {title: "I enjoy coding as a way to feel calm", body: "This is my first blog post!"}
+  {title: "Why I'm teaching myself React", body: "React is a popular language for front-end development. I'm learning it to become a better developer and designer."},
+  {title: "My first week learning ReactJS", body: "So far I've learned about components, props, and JSX. I'm excited to learn more!"},
+  {title: "I built a simple blog", body: "I built this blog using ReactJS. I'm excited to continue learning!"},
+  {title: "I enjoy coding as a way to feel calm", body: "Coding brings me a sense of calm and peace. I'm excited to continue learning!"},
 ];
 
 
